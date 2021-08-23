54621861a6becdadecc14a4ee351a4321276497e
nilsonsasaki <nilsonsasaki@gmail.com>
2021-08-23 11:14:19 -0300

    updated reset order to public

--- app/src/main/java/com/example/cupcake/model/OrderViewModel.kt
+++ app/src/main/java/com/example/cupcake/model/OrderViewModel.kt
@@ -23,11 +23,11 @@ class OrderViewModel : ViewModel() {
     val date: LiveData<String> = _date
 
     private val _price = MutableLiveData<Double>()
-    val price: LiveData<String> = Transformations.map(_price){
+    val price: LiveData<String> = Transformations.map(_price) {
         NumberFormat.getCurrencyInstance().format(it)
     }
 
-    val dateOptions:List<String> = getPickupOptions()
+    val dateOptions: List<String> = getPickupOptions()
 
     init {
         resetOrder()
@@ -61,7 +61,8 @@ class OrderViewModel : ViewModel() {
         }
         return options
     }
-    private fun resetOrder() {
+
+    fun resetOrder() {
         _quantity.value = 0
         _flavor.value = ""
         _date.value = dateOptions[0]
